51f078c21e53bab66697ae258e3550e8625d29b5
lsantos92 <luis.santos.job@gmail.com>
2022-10-13 20:37:09 +0100

    Initial Commit

---
+++ singletriangle/singletriangle.cpp
@@ -0,0 +1,8 @@
+//
+//  singletriangle.cpp
+//  opengl_cg
+//
+//  Created by Luís Santos on 13/10/2022.
+//
+
+#include <stdio.h>
---
+++ singlewindow/singlewindow.cpp
@@ -0,0 +1,8 @@
+//
+//  singlewindow.cpp
+//  opengl_cg
+//
+//  Created by Luís Santos on 13/10/2022.
+//
+
+#include <stdio.h>
