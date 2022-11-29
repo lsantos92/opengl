07629f11c8a5fdeeb0932faecfd55bdff330ef33
Luís Santos <luis.santos.job@gmail.com>
2022-11-29 11:49:14 +0000

    final exercise

--- lighting/main.cpp
+++ lighting/main.cpp
@@ -20,7 +20,7 @@ const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 // camera
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+Camera camera(glm::vec3(0.0f, 0.0f, 5.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
@@ -187,8 +187,8 @@ int main()
         
         // change the light's position values over time (can be done anywhere in the render loop actually, but try to do it at least before using the light source positions)
       
-        lightPosition.x =sin(glfwGetTime()) * 2.0f;
-        lightPosition.y =cos(glfwGetTime()/2.0f) * 1.0f;
+        //lightPosition.x =sin(glfwGetTime()) * 2.0f;
+        //lightPosition.y =cos(glfwGetTime()/2.0f) * 1.0f;
     
       
         
